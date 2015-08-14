3aad37ee19290be5897ebddfce21d85812019485
ranjeet <amitlranjeet_5619@yahoo.co.in>
2015-08-14 01:35:02 -0800

    sprint-3 branch commit

--- session_without_cookie.php
+++ session_without_cookie.php
@@ -1,6 +1,7 @@
 <?php
 /**
 This is the example of testing branch merging for hotfix bla bla ...
+This is the example of testing branch merging for hotfix bla bla ...
 ***/
 session_start();
 echo session_id();
